60cf5d39b0a03d48158e70755170184cc802c134
IrsyadHusainJauhari123 <108142445+IrsyadHusainJauhari123@users.noreply.github.com>
2022-08-31 00:46:00 +0700

    Initial commit

---
+++ system/app/Models/produk.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+class produk extends Model
+{
+    protected $table = 'produk';
+
+    function seller()
+    {
+        return $this->Belongsto(User::class, 'id_user');
+    }
+}
